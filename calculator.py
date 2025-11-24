2aa66635f0461ec8621c2540417f2cf0ecbf8c01
Nushervon Zubaydulloev <nushervon.z@gmail.com>
2025-11-24 21:32:00 +0500

    calculator added

---
+++ calculator.py
@@ -0,0 +1,10 @@
+class Calculator():
+    def sum(a,b):
+        return a+b
+    def extract(a,b):
+        return sum(a,-b)
+    def multiply (a,b):
+        return a*b
+    def devide(a,b):
+        return a/b
+    
